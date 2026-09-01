9f3ce71f0924f10e82acb7704e18bdae51528e94
Benedikt Wiedemann <benedikt.wiedemann@uni-ulm.de>
2026-09-01 10:02:34 +0200

    add difference_plots

--- create_manuscript_plots.py
+++ create_manuscript_plots.py
@@ -5,6 +5,7 @@ from visualization.bar_plot_screened_cands import create_bar_plot_screened_cands
 from visualization.ray_independence_plots import create_ray_independence_plots_SI
 from visualization.cand_plots import plot_cand
 from visualization.pareto_plots_H_vs_E_ratio import create_pareto_8_leds_13cm, create_pareto_1_16_leds_5_15cm
+from visualization.difference_plots import create_difference_plots_SI
 
 if __name__ == "__main__":
     # plot_cand("best_manual") # uncomment to generate plot of best manual configuration
@@ -16,3 +17,4 @@ if __name__ == "__main__":
     create_bar_plot_final_comparison() # uncomment to generate final comparison bar plot
     # create_ray_independence_plots_SI() # uncomment to generate SI ray independence plots
     # create_irradiance_plots_SI() # uncomment to generate SI irradiance plots
+    # create_difference_plots_SI() # uncomment to generate SI difference plots
